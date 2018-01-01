2f0b0ceab45a2183b0563009ae4e6ebd5bbb5463
CXian <chenxiannn@126.com>
2018-01-02 00:08:38 +0800

    Init Process Txt
    初始化代码，处理txt图像

---
+++ Graphic/imCom.c
@@ -0,0 +1,16 @@
+#include "imCom.h"
+
+imINT32 int_min(imINT32 a,imINT32 b)
+{
+    if (a < b)
+      return a;
+    else 
+      return b;
+}
+ imINT32 int_max(imINT32 a,imINT32 b)
+{
+    if (a > b)
+      return a;
+    else 
+      return b;
+}
